7e670aec82b297d79ca528fb3f5d3b5fb4e17917
atpur-rafif <muhammadatpurrafif@gmail.com>
2024-08-22 21:12:18 +0700

    Default admin user

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,20 @@ import "./route/user-view";
 import "./route/admin";
 import { dataSource } from "./entity/config";
 import { parse } from "url";
+import { adminRepository } from "./entity/repository";
 
 async function main() {
 	await dataSource.initialize();
+
+	const admin = adminRepository.findBy({ username: "admin" })
+	if (!admin) {
+		adminRepository.create({
+			username: "admin",
+			password: "adminadmin",
+			email: "admin@example.com",
+		})
+	}
+
 	const server = createServer({}, (req, res) => {
 		res.setHeader("Access-Control-Allow-Origin", "*");
 		res.setHeader("Access-Control-Allow-Methods", "*");
