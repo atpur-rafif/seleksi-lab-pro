7a644eba5a67c714537e95df832e47f29766bf8d
atpur-rafif <muhammadatpurrafif@gmail.com>
2024-08-22 21:14:32 +0700

    Default page

--- src/route/index.ts
+++ src/route/index.ts
@@ -3,6 +3,12 @@ import { Router, RouterError } from "../module/router";
 import { createReadStream, existsSync } from "fs";
 export const router = new Router();
 
+router.defineRoute("GET", "/", async (req, res) => {
+	res.statusCode = 303
+	res.setHeader("Location", "/browse")
+	res.end();
+})
+
 router.defineRoute("GET", "/static/*", async (req, res) => {
 	const filename = basename(req.pathname);
 	const path = resolve("static", filename);
